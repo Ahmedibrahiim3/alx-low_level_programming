a404d7142ed3c0587678cc1afa87814176c434fb
ahmedibrahiim3 <ahmedibrahiim37@gmail.com>
2023-06-16 11:18:08 -0700

    trying my best at betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main -Entery point
  *
- * Description: using put function for printing
+ * Description: print with puts
  *
- * Return: Always 0 (Success)
- * 
+ * return: 0 (success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
