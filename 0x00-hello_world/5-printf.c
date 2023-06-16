7457288e04b0807f30cb0010e18cb7c25ba5429a
ahmedibrahiim3 <ahmedibrahiim37@gmail.com>
2023-06-15 19:57:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Desciption: using a printf to print some input
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
