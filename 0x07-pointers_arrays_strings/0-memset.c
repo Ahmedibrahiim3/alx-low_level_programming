58248b95ee7488e0899c8e78f94aea18ec03b033
ahmedibrahiim3 <ahmedibrahiim37@gmail.com>
2023-07-03 16:25:53 -0700

    pointers of pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,20 +2,21 @@
 
 /**
  * _memset - take portion of memory and fill
- * @s: starting point of memory
+ * @s: starting point of memory to be filled
  * @b: value we will put
  * @n: number of bytes we will take
  *
  * Return: changing for array with new values with n bytes
 */
 
-char *_memset(char *S, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
+
 	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 	}
-	return (s);	
+	return (s);
 }
