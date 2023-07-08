64e5da02458d4d59a9a3ae5285c4a01295038bf1
ahmedibrahiim3 <ahmedibrahiim37@gmail.com>
2023-07-07 19:26:11 -0700

    function

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: number of times
+ * Return: for no number \n number of _ witt \n at the end
+*/
+
+void print_line(int n)
+{
+	int no_of_n;
+
+	if (n <= 0)
+		_putchar ('\n');
+	else
+	{
+		for (no_of_n = 1; no_of_n <= n;)
+			_putchar ('_');
+		_putchar ('\n');
+	}
+}
