375dfcdba57fa969b09a1b1877bcc4969af4a26d
ahmedibrahiim3 <ahmedibrahiim37@gmail.com>
2023-07-11 20:41:01 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - that give char c to stdout.
+ * @c: char that will be printed
+ *
+ * Return: 1 Success
+ *        -1 Error
+*/
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
