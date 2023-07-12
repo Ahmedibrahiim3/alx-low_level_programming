b16b2d0e644f13772c6e920a36d4b91506e4de6d
ahmedibrahiim3 <ahmedibrahiim37@gmail.com>
2023-07-12 13:45:22 -0700

    argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints arguments passed
+ * @argc: int
+ * @argv: array
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int total;
+
+	if (argc == 3)
+	{
+		total = atoi(argv[i]) * atoi(argv[2]);
+		printf("%d\n", total);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
